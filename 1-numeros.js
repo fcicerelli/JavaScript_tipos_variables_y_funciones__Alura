f49f09910dd76325e09ae072a6caf8365c26d797
fcicerelli <fcicerellitic@gmail.com>
2025-02-20 11:18:53 -0300

    tipos de variable number y String

---
+++ 1-numeros.js
@@ -0,0 +1,24 @@
+var primerNumero = 3.5;
+var segundoNumero = 25.5;
+
+// Operaciones aritméticas con números
+var sumaNumeros = primerNumero + segundoNumero;
+var restaNumeros = primerNumero - segundoNumero;
+var multiplicacionNumeros = primerNumero * segundoNumero;
+var divisionNumeros = primerNumero / segundoNumero;
+
+console.log('La suma fue: ',sumaNumeros);
+console.log('La resta fue: ',restaNumeros);
+console.log('La multiplicacion fue: ',parseInt(multiplicacionNumeros));
+console.log('La division fue: ',divisionNumeros.toFixed(2));
+
+var numeroMasLetras = primerNumero + "AluraLatam";
+console.log(numeroMasLetras); 
+
+var a = 10
+var b = 0
+console.log(a/b) // Infinity
+
+var c = 0
+var d = 0
+console.log(c/d) // NaN
---
+++ 2-Strings.js
@@ -0,0 +1,34 @@
+var nombreCompleto = 'Leonardo José';
+var ciudadDomicilio = "Buenos Aires";
+var fechaNacimiento = "13 de noviembre de 2008";
+var diaIndependenciaPais = "19/04/1810";
+
+console.log(nombreCompleto);
+console.log(ciudadDomicilio);
+console.log(fechaNacimiento);
+console.log(diaIndependenciaPais);
+
+var fichaTecnicaModo1 = "Modo 1:\n\
+Nombre del producto\n\
+Código del producto\n\
+Valor";
+
+console.log(fichaTecnicaModo1);
+
+var fichaTecnicaModo2 = "Modo 2:\n" +
+"Nombre del producto\n" +
+"Código del producto\n" +
+"Valor";
+
+console.log(fichaTecnicaModo2);
+
+var fichaTecnicaModo3 = `Modo 3:
+Nombre del producto
+Código del producto
+Valor`;
+
+console.log(fichaTecnicaModo3);
+
+console.log(nombreCompleto.toLowerCase());
+console.log(ciudadDomicilio.toUpperCase());
+console.log(fechaNacimiento.length);
